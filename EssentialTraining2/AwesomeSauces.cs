bca65308ca00c6e26a8d2968a3ab1dd16881c830
Adam Batson <tabatson@gmail.com>
2021-08-30 17:20:00 -0500

    Add all files from LinkedIn Learning Course

---
+++ EssentialTraining2/AwesomeSauces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EssentialTraining2
+{
+    public class AwesomeSauces
+    {
+        public List<string> Sauces { get; set; }
+        public AwesomeSauces()
+        {
+            Sauces = new List<string>();
+        }
+        public bool IsSauceAwesome(string sauce)
+        {
+            return Sauces.Contains(sauce);
+        }
+
+    }
+}
---
+++ EssentialTrainingTests/AwesomeSaucesTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using EssentialTraining2;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class AwesomeSaucesTest
+    {
+        [TestMethod]
+        public void IsSauceAwesomeTest()
+        {
+            var testInstance = new AwesomeSauces();
+            testInstance.Sauces.Add("Tabasco");
+            testInstance.Sauces.Add("Cholula");
+            testInstance.Sauces.Add("Trailer Trash");
+
+            //expect to pass
+            Assert.IsTrue(testInstance.IsSauceAwesome("Trailer Trash"));
+
+            //expect to fail
+            Assert.IsFalse(testInstance.IsSauceAwesome("Ketchup"));
+        }
+    }
+}
